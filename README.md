b8ea381621b49bae47f81f42acf5d1b6a90dd06e
Maximiliano Juarez <maximiliano.juarez05@gmail.com>
2025-03-22 16:23:30 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# luz
+
+Este es un proyecto React creado con un generador personalizado.
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <section>
+    <div className='botonera'>
+      <button className='prende'>Encender</button>
+      <button className='apaga'>Apagar</button>
+    </div>
+    </section>
+  );
+}
+
+export default App;
