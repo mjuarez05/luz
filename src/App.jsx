e7b661b0f2c2369e4eb26bb1e5d4d14641bdae43
Maximiliano Juarez <87102081+mjuarez05@users.noreply.github.com>
2025-03-22 21:20:10 -0300

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
 function App() {
+const [estado, setEstado] = useState("apagada");
+const RASPBERRY_IP = "http://localhost:5000";
+
+const controlarLampara = async (accion) => {
+    try {
+      const response = await axios.get(`${RASPBERRY_IP}/${accion}`);
+      setEstado(response.data.status);
+    } catch (error) {
+      console.error("Error al conectar con la Raspberry", error);
+    }
+  };
+
   return (
     <section>
     <div className='botonera'>
-      <button className='prende'>Encender</button>
-      <button className='apaga'>Apagar</button>
+      <button className='prende'onClick={() => controlarLampara("encender")}>Encender</button>
+      <button className='apaga'prende'onClick={() => controlarLampara("apagar")}>Apagar</button>
     </div>
     </section>
   );
