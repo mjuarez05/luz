bfaaf34269ba5d2fecf2bc23865558535b4bb7b7
Maximiliano Juarez <maximiliano.juarez05@gmail.com>
2025-03-29 13:44:49 -0300

    Agregar dependencia de axios y mejorar el estilo de la lámpara en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-const [estado, setEstado] = useState("apagada");
-const RASPBERRY_IP = "http://192.168.0.221:5000";
+  const [estado, setEstado] = useState("apagada");
+  const RASPBERRY_IP = "http://192.168.0.221:5000";
 
-const controlarLampara = async (accion) => {
+  const controlarLampara = async (accion) => {
     try {
       const response = await axios.get(`${RASPBERRY_IP}/${accion}`);
       setEstado(response.data.status);
@@ -16,10 +16,21 @@ const controlarLampara = async (accion) => {
 
   return (
     <section>
-    <div className='botonera'>
-      <button className='prende'onClick={() => controlarLampara("encender")}>Encender</button>
-      <button className='apaga'onClick={() => controlarLampara("apagar")}>Apagar</button>
-    </div>
+      <div className={`foco ${estado === "encendida" ? "foco-encender": "foco-apagar"}`}>
+        <div className='base-filamento'></div>
+        <div className='filamento'></div>
+
+        <div className='base'></div>
+      </div>
+
+      <div className='botonera'>
+        <button className='prende' onClick={() => controlarLampara("encender")}>
+          Encender
+        </button>
+        <button className='apaga' onClick={() => controlarLampara("apagar")}>
+          Apagar
+        </button>
+      </div>
     </section>
   );
 }
